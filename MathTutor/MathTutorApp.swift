6af8abe9b5959d48474f064076e2e12f42b441c7
Bob Beachill <bbeachill@icloud.com>
2025-03-06 17:45:39 +0000

    Initial Commit

---
+++ MathTutor/MathTutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathTutorApp.swift
+//  MathTutor
+//
+//  Created by Robert Beachill on 06/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct MathTutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
